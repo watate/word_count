9a2a3197f23010b1e055cc006da213e8b10948c0
Walter Tay Ann Lee <40448679+watate@users.noreply.github.com>
2025-07-25 14:25:27 +0800

    Initial commit

---
+++ example/word_count_example.dart
@@ -0,0 +1,6 @@
+import 'package:word_count/word_count.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/word_count_test.dart
@@ -0,0 +1,16 @@
+import 'package:word_count/word_count.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
